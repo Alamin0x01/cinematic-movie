ec833866cecc44e9a4ccfb83784be612248cb2cf
Md. Alamin A <86530759+Alamin0x01@users.noreply.github.com>
2023-11-05 09:47:56 +0600

    register  page added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 import MenuBar from '../components/MenuBar';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+
 import { SignUp } from '@clerk/nextjs';
 
 const page = () => {
